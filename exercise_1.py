4721be35ef156a9c6b48c35f2ec172115a091f17
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-11 17:13:49 +0530

    Create exercise_1.py

---
+++ exercise_1.py
@@ -0,0 +1,2 @@
+#Q1) Print the twinkle twinkle little star line with new lines after every phrase
+print("Twinkle Twinkle little star \nHow I wonder what you are \nUp above the world so high \nLike a diamond in the sky")
