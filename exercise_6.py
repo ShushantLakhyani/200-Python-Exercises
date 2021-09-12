aa09fa8fcb7f3a735c3319f2b3f17658c425ef1b
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-12 17:04:28 +0530

    Create exercise_6.py

---
+++ exercise_6.py
@@ -0,0 +1,12 @@
+# Write a Python program to construct the following pattern, using a nested loop number.
+# 1                                                                                                             
+# 22                                                                                                            
+# 333                                                                                                           
+# 4444                                                                                                          
+# 55555                                                                                                         
+# 666666                                                                                                        
+# 7777777                                                                                                       
+# 88888888                                                                                                      
+# 999999999
+for i in range(10):
+    print(str(i) * i)
