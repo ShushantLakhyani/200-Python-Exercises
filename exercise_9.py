34f50fe87a620c1aa0fcd24bc3d8eb963dfd140c
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-12 17:30:35 +0530

    Create exercise_9.py

---
+++ exercise_9.py
@@ -0,0 +1,14 @@
+# Count the number of even and odd numbers from a series of numbers
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+#initialise the variables (to count the number of even and odd numbers) to zero
+count_odd = 0
+count_even = 0
+#An if-else loop nested in a for loop to count the number of even and odd numbers
+for x in numbers:
+        if not x % 2:
+        count_even+=1
+        else:
+        count_odd+=1
+#print the number of even and odd numbers                
+print("Number of even numbers :",count_even)
+print("Number of odd numbers :",count_odd)
