9ce3f6f26e49bcfa03b895f2f29247d3d29cd347
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-12 17:31:12 +0530

    Adding indentation to code

--- exercise_9.py
+++ exercise_9.py
@@ -6,9 +6,9 @@ count_even = 0
 #An if-else loop nested in a for loop to count the number of even and odd numbers
 for x in numbers:
         if not x % 2:
-        count_even+=1
+                count_even+=1
         else:
-        count_odd+=1
+                count_odd+=1
 #print the number of even and odd numbers                
 print("Number of even numbers :",count_even)
 print("Number of odd numbers :",count_odd)
