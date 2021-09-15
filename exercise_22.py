f5bd859909f6de6f3c4c503efe803d26d3c811af
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-15 09:38:59 +0530

    Create exercise_22.py

---
+++ exercise_22.py
@@ -0,0 +1,5 @@
+#  Write a Python program to generate all permutations of a list in Python
+# Input [1,2,3]
+# Output [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
+import itertools
+print(list(itertools.permutations([1,2,3])))
