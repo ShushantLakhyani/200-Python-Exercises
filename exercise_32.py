e96a875e2a27c0e0646db9241a72eddef9bc8785
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-17 09:28:30 +0530

    Create exercise_32.py

---
+++ exercise_32.py
@@ -0,0 +1,10 @@
+#revert a sentence: 'The quick brown fox jumps over the lazy dog.'
+# input : "The quick brown fox jumps over the lazy dog."
+# output : "dog. lazy the over jumps fox brown quick The "
+
+def reverse_string_words(text):
+    for line in text.split('\n'):
+        return(' '.join(line.split()[::-1]))
+    
+print(reverse_string_words("The quick brown fox jumps over the lazy dog."))
+print(reverse_string_words("Python Exercises."))    
