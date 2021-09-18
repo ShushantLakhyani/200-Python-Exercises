af746ccfcb22390bad303f2c2480e75b1811156e
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-18 18:01:34 +0530

    Create exercise_39.py

---
+++ exercise_39.py
@@ -0,0 +1,9 @@
+# Q2) Write a python program to add two numbers
+
+#SOLUTION:
+#declare two variables, lets say 'a' and 'b', and assign a value to them
+#now declare a new variable, lets say this variable is 'c'. Assign 'c'  to the sum of 'a' and 'b', and your problem is solved
+a = 1
+b = 2
+c = a + b
+print(c)
