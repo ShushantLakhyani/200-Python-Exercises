00cbf0f3171dd2cb317ab1cdd29ce301dab7fa70
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-18 18:10:15 +0530

    Create square_root__positive_number.py

---
+++ square_root__positive_number.py
@@ -0,0 +1,10 @@
+# Q3) Find the square root of a positive number
+
+#Declare a number in a variable
+a = 8
+
+#to find thhe square root we raise the number to the power of 0.5, so raise a to the power of 0.5
+a = a ** 0.5
+
+# Now, print a to get the square root
+print(a)
