19e3d9ce4d66e56acf41d7fdf658c70ae5578bbf
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-18 20:48:29 +0530

    Create sq_root_complex_number.py

---
+++ sq_root_complex_number.py
@@ -0,0 +1,14 @@
+# Q4) Find the square root of a real or a complex number
+
+#SOLUTION:
+#import the complex numbers math module
+import cmath
+
+#let us have a complex number declared in a variable
+num = 1 + 2j
+
+#calculate the square root using .sqrt
+num_sqrt = cmath.sqrt(num)
+
+#print the square root
+print(num_sqrt)
